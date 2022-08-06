bd53d9170d2bedc248d37358e3b7da532bb8009b
sleep <wjs09211>
2022-08-06 16:07:50 +0800

    add log

--- api/trainCrawler/trainCrawler.py
+++ api/trainCrawler/trainCrawler.py
@@ -114,6 +114,7 @@ class TrainCrawler:
         url = HOST + '/tra-tip-web/ptr/pts131/qryseat/time'
         self._setting_secret_key()
         r = self.session.post(url, json=[post_data], headers=self.header)
+        logging.info('query_exist_seat')
 
         remain_seat = json.loads(r.text)
         if remain_seat['message'] == 'OK':
